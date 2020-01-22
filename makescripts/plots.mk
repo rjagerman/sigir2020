8701c4adf80d0fbfacccf6547028d1513e588f72
Rolf Jagerman <rjagerman@gmail.com>
2020-01-22 16:05:52 +0100

    Update plot make scripts to adjust height to fit paper

--- makescripts/plots.mk
+++ makescripts/plots.mk
@@ -7,7 +7,7 @@ $(BUILD)/plots/batchsizes.pdf : | yahoo_batch_sizes_repeat_5 istella_batch_sizes
 		--dataset test --json_files ./results/batch_sizes/* \
 		--out $@.tmp.pdf \
 		--format pdf \
-		--legend --height 3.4
+		--legend --height 3.39
 	mv $@.tmp.pdf $@
 
 $(BUILD)/plots/optimizers.pdf : | yahoo_optimizers_repeat_5 istella_optimizers_repeat_5 $(BUILD)/plots/
@@ -15,7 +15,7 @@ $(BUILD)/plots/optimizers.pdf : | yahoo_optimizers_repeat_5 istella_optimizers_r
 		--dataset test --json_files ./results/optimizers/* \
 		--out $@.tmp.pdf \
 		--format pdf \
-		--legend --height 3.4
+		--legend --height 3.39
 	mv $@.tmp.pdf $@
 
 $(BUILD)/plots/etas.pdf : | yahoo_etas_repeat_5 istella_etas_repeat_5 $(BUILD)/plots/
@@ -23,7 +23,7 @@ $(BUILD)/plots/etas.pdf : | yahoo_etas_repeat_5 istella_etas_repeat_5 $(BUILD)/p
 		--dataset test --json_files ./results/etas/* \
 		--out $@.tmp.pdf \
 		--format pdf \
-		--legend --height 3.4
+		--legend --height 3.39
 	mv $@.tmp.pdf $@
 
 
